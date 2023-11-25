fef455f53c7c8df2178cd1269a3a2f21b904cb58
arya <arya.abawi@gmail.com>
2023-11-25 13:18:53 -0500

    adding node registration to Breez component

--- app/components/Breez.tsx
+++ app/components/Breez.tsx
@@ -1,26 +1,34 @@
-import { EnvironmentType, NodeConfig, NodeConfigType, connect, defaultConfig, mnemonicToSeed } from '@breeztech/react-native-breez-sdk';
+import { EnvironmentType, NodeConfig, NodeConfigVariant, connect, defaultConfig, mnemonicToSeed, BreezEvent, nodeInfo } from '@breeztech/react-native-breez-sdk';
 import { MNEMONIC_PHRASE, BREEZ_INVITE_CODE, BREEZ_API_KEY } from "@env"
 
 async function BreezNode() {
+    const onBreezEvent = (e: BreezEvent) => {
+        console.log(`Received event ${e.type}`)
+    }
+
     const phrase = MNEMONIC_PHRASE;
     const inviteCode = BREEZ_INVITE_CODE;
     const apiKey = BREEZ_API_KEY;
 
     const seed = await mnemonicToSeed(phrase);
-    console.log(seed);
-    // const nodeConfig : NodeConfig = {
-    // type: NodeConfigType.GREENLIGHT,
-    // config: {
-    //     inviteCode: inviteCode
-    //     }
-    // }
-    // let config = await defaultConfig(EnvironmentType.PRODUCTION, apiKey, nodeConfig);
+    const nodeConfig : NodeConfig = {
+        type: NodeConfigVariant.GREENLIGHT,
+        config: {
+            inviteCode
+        }
+    }
+    let config = await defaultConfig(EnvironmentType.PRODUCTION, apiKey, nodeConfig);
 
-    // try {
-    //     const sdkServices = await connect(config, seed);
-    // } catch (error) {
-    //     console.log(error)
-    // }
+    try {
+        await connect(config, seed, onBreezEvent);
+        const nodeState = await nodeInfo();
+        const balanceLn = nodeState.channelsBalanceMsat
+        const balanceOnchain = nodeState.onchainBalanceMsat
+        console.log(`LN Balance: ${balanceLn}`);
+        console.log(`On Chain Balance: ${balanceOnchain}`);
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export default BreezNode
