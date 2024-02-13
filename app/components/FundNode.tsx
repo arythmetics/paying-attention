21ba65083de704081e6e8358e43ed3084cf4c08e
arya <arya.abawi@gmail.com>
2024-02-13 10:22:20 -0500

    added buy bitcoin link from Moonpay to FundNode component

--- app/components/FundNode.tsx
+++ app/components/FundNode.tsx
@@ -1,11 +1,23 @@
 import { Text } from 'react-native'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { BreezNodeContext } from './Breez'
+import { BuyBitcoinProvider, buyBitcoin } from '@breeztech/react-native-breez-sdk'
 
 export default function FundNode (): React.ReactElement {
+  const [buyBitcoinUrl, setBuyBitcoinUrl] = useState<string | null>(null)
   const nodeState = useContext(BreezNodeContext)
-  const nodeBalance = nodeState?.channelsBalanceMsat
+  const nodeBalance = nodeState?.channelsBalanceMsat;
+  
+  useEffect(() => {
+      buyBitcoin({
+        provider: BuyBitcoinProvider.MOONPAY
+      })
+      .then(res => setBuyBitcoinUrl(res.url))
+      .catch(err => console.error(err))
+  }, []);
+  
   return <Text>
         {nodeBalance ?? "loading..."}
+        {buyBitcoinUrl ?? "loading..."}
     </Text>
 }
